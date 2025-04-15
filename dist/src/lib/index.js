a0835f6b454679a5f64845b1137bdfc06ad9c933
medvall01 <medvallmougder@yahoo.com>
2025-04-15 12:14:32 +0100

    fix: add compiled app.js

---
+++ dist/src/lib/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.displayUserName = void 0;
+const displayUserName = (user) => {
+    return `${user.firstName} ${user.lastName}`;
+};
+exports.displayUserName = displayUserName;
